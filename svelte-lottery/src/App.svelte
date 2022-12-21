54c4217d52cf752e40723659c967790e4477fd68
Julian Hamre <julian.hamre5@gmail.com>
2022-12-21 10:38:05 +0100

    Add selectable number buttons
    You can press numbered buttons to store or discard the number in the selected_numbers object
    - Add selected_numbers class
    - Add range function
    - Add loop which divides buttons into selected (red) and not selected
    - most css from default svelte install

---
+++ svelte-lottery/src/App.svelte
@@ -0,0 +1,64 @@
+<script>
+  class selected_numbers {
+    constructor() {
+      this.selected = [];
+    }
+
+    get() {
+      return this.selected;
+    }
+
+    in(number) {
+      return this.selected.includes(number);
+    }
+
+    add(number) {
+      this.selected.push(number);
+      this.selected = this.selected;
+      return this;
+    }
+
+    remove(number) {
+      let index = this.selected.indexOf(number);
+      this.selected.splice(index, 1);
+      return this;
+    }
+
+    is_complete() {
+      if (this.selected >= 7) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  function range(size, start_at) {
+    return [...Array(size).keys()].map((i) => i + start_at);
+  }
+
+  let loops = range(34, 1);
+  let numbers = new selected_numbers();
+</script>
+
+<p>
+  {numbers.get()}
+</p>
+
+<p>
+  {#each loops as i}
+    {#if numbers.selected.includes(i)}
+      <button
+        class="selected"
+        on:click={() => {
+          numbers = numbers.remove(i);
+        }}>{i}</button
+      >
+    {:else}
+      <button
+        on:click={() => {
+          numbers = numbers.add(i);
+        }}>{i}</button
+      >
+    {/if}
+  {/each}
+</p>
